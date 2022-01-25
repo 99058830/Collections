5affca2672049f3b5d607f4f9a4ec232834637d6
99058830 <99058830@mydavinci.nl>
2022-01-25 11:21:27 +0100

    fixed typos

---
+++ mmDict.py
@@ -0,0 +1,26 @@
+from random import randint
+import random
+
+mmList = ['oranje','groen','blauw','bruin']
+mmDictionary = {}
+
+def mmDict(amount):
+    for i in range(amount):
+        randomIndex = random.randrange(0, len(mmList))
+        color = mmList[randomIndex]
+        if color in mmDictionary:
+            mmDictionary[color] += 1
+        else:
+            mmDictionary[color] = 1
+    return mmDictionary
+
+def main():
+    global mmSelect
+    amount = int(input("hoeveel M&M's? "))
+    if amount > 0:
+        print(mmDict(amount))
+    elif type(amount):
+        print('Geen getal') 
+        main()
+
+main()
